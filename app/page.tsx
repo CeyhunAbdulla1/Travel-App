d5ae6c344aff481070cd2adcf184ebad5beb438c
CeyhunAbdulla1 <https://github.com/CeyhunAbdulla1/Travel-App>
2023-11-27 23:15:45 +0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import Hero from './components/Hero'
+import Guide from './components/Guide'
+import Camp from './components/Camp'
+import Features from './components/Features'
+import GetApp from './components/GetApp'
+
+export default function Home() {
+  return (
+    <>
+      <Hero/>
+      <Camp/>
+      <Guide/>
+      <Features/>
+      <GetApp/>
+    </>
+  )
+}
